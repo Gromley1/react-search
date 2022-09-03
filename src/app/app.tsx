1727131bbbebd3ad24fdf1d6b7b60b2864eaad36
gromley111 <gromley111@gmail.com>
2022-09-03 13:12:58 -0700

    first commit

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -8,31 +8,7 @@ function App() {
 	
 	return (
     	<div className={styles.app}>
-			<div className={styles.app__headings}>
-			<AppHeading as='h2' size={2}>App heading H2</AppHeading>
-			<AppHeading as='h3' size={3}>App heading H3</AppHeading>
-			</div>
-
-			<div className={styles.app__actions}>
-				<AppButton
-					style={ButtonStyle.inverted}
-					onClick={(): void => console.log('btn inverted click')}
-				>
-					App Button Inverted
-				</AppButton>
-
-				<AppButton style={ButtonStyle.default} onClick={(): void => console.log('default')}>
-					App Button Default
-				</AppButton>
-
-				<AppButton
-					style={ButtonStyle.inverted}
-					rounded
-					onClick={(): void => console.log('btn rounded inverted click')}
-				>
-					App Button Inverted
-				</AppButton>
-			</div>
+		
     	</div>
 	);
 }
